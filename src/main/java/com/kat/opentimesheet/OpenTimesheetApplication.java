eba892c11952bf4f5b297e22239a49f11aa43882
Phạm Huy Đức <ducph@ominext.com>
2018-05-14 09:04:59 +0700

    - First add Source + Using Openfaces

---
+++ src/main/java/com/kat/opentimesheet/OpenTimesheetApplication.java
@@ -0,0 +1,12 @@
+package com.kat.opentimesheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenTimesheetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenTimesheetApplication.class, args);
+    }
+}
